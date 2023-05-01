ed4de26542bd45d440d896eaf6d7d7230b26fc55
ketexon <zane.a.s.clark@gmail.com>
2023-04-30 19:06:35 -0700

    made w/h const

--- Source/main.lua
+++ Source/main.lua
@@ -1,8 +1,8 @@
 import "CoreLibs/timer"
 import "CoreLibs/easing"
 
-local WIDTH = 400
-local HEIGHT = 240
+local WIDTH <const> = 400
+local HEIGHT <const> = 240
 
 local font = playdate.graphics.getSystemFont("bold")
 
@@ -11,24 +11,24 @@ local text ="If the fish isn't hooked, you can't reel it in"
 local text_width =font:getTextWidth(text)
 local text_height =font:getHeight()
 
-local text_position_y = HEIGHT 
+local text_position_y = HEIGHT
 local text_target_position_y = HEIGHT/2 - text_height/2
 
 local reeling_em_in = playdate.timer.new(
-    1000, 
-    HEIGHT, 
+    1000,
+    HEIGHT,
     HEIGHT/2 - text_height/2,
     playdate.easingFunctions.inExpo
 )
 
-local hook = playdate.graphics.image.new("Hook.pdi") 
+local hook = playdate.graphics.image.new("Hook.pdi")
 
 function playdate.update()
     playdate.graphics.clear()
-    
+
     playdate.graphics.drawText(
         "If the fish isn't hooked, you can't reel it in",
-         WIDTH/2 - text_width/2, 
+         WIDTH/2 - text_width/2,
          reeling_em_in.value
     )
 
